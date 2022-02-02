6f20f4100c0e6b467a5f722f0de245610e292dc6
Sina Shateri <sina_sh_79@yahoo.com>
2022-02-03 00:49:21 +0330

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { motion } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+import images from "./image-import";
+import "./app.css";
 
 const App = (props) => {
-  return <div></div>;
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+  return (
+    <div>
+      <motion.div
+        ref={carousel}
+        className="carousel"
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
+          {images.map((image) => (
+            <motion.div className="item" key={image}>
+              <img src={image} alt={image} />
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/image-import.js
@@ -0,0 +1,7 @@
+import image1 from "./assets/pexels-1.jpg";
+import image2 from "./assets/pexels-2.jpg";
+import image3 from "./assets/pexels-3.jpg";
+import image4 from "./assets/pexels-4.jpg";
+import image5 from "./assets/pexels-5.jpg";
+
+export default [image1, image2, image3, image4, image5];
